f06f90127609b29462a532cb387d9801ea13d748
Cleverton <clevertonmaggot@gmail.com>
2016-09-15 22:37:18 -0300

    Testes da filtragem de métodos longos pelos tipos de abordagem: Valor limiar fixo, projeto exemplo sem utilizar interesses arquiteturais e projeto de exemplo utilizando as preocupações arquiteturais.

--- test/br/ufs/smelldetector/negocio/FiltrarMetodosLongosTest.java
+++ test/br/ufs/smelldetector/negocio/FiltrarMetodosLongosTest.java
@@ -1,43 +1,91 @@
 package br.ufs.smelldetector.negocio;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import org.junit.Test;
 
 import br.ufs.smelldetector.model.DadosClasse;
+import br.ufs.smelldetector.model.DadosComponentesArquiteturais;
 import br.ufs.smelldetector.model.DadosMetodoLongo;
 
 public class FiltrarMetodosLongosTest {
 
-	@Test
-	public void testFiltrarPorValorLimiar() {
-		ArrayList<String> projetos = new ArrayList<String>();
-		projetos.add("D:/Projetos/mobilemedia/MobileMedia01_OO");
-		
-		AnalisadorProjeto analisadorProjeto = new AnalisadorProjeto();
-		ArrayList<DadosClasse> classes = analisadorProjeto.getInfoMetodosPorProjetos(projetos, false);
-		
-		FiltrarMetodosLongos filtrarMetodosLongos = new FiltrarMetodosLongos();
-		ArrayList<DadosMetodoLongo> metodosLongos = filtrarMetodosLongos.filtrarPorValorLimiar(classes, 40);
-		
+	private final int porcentagem = 75;
+	private final int valorLimiar = 10;
+	
+	private String projetoExemplo = "C:/Users/Kekeu/Documents/mobilemedia/MobileMedia01_OO"; 
+	
+	private ArrayList<String> getProjetosParaAnalise() {
+		ArrayList<String> projetosParaAnalise = new ArrayList<String>();
+		projetosParaAnalise.add(projetoExemplo);
+		return projetosParaAnalise;
+	}
+	
+	private void exibeMetodosLongos(ArrayList<DadosMetodoLongo> metodosLongos, 
+			String descricaoMetodo) {
+		System.out.println("\n\n\n"+descricaoMetodo);
 		System.out.println("Classe   |     Método       |     Linhas Código");
 		for (DadosMetodoLongo metodoLongo: metodosLongos) {
-			System.out.println(metodoLongo.getNomeClasse() + "  | " + metodoLongo.getNomeMetodo() + " | " + metodoLongo.getNumeroLinhas() );
+			System.out.println(metodoLongo.getNomeClasse() + "  | " + 
+					metodoLongo.getNomeMetodo() + " | " + metodoLongo.getNumeroLinhas() );
 		}
+		System.out.println("Total de métodos longos: "+metodosLongos.size());
+	}
+	
+	@Test
+	public void testFiltrarPorValorLimiar() {
+		AnalisadorProjeto analisadorProjeto = new AnalisadorProjeto();
+		ArrayList<DadosClasse> classes = analisadorProjeto.getInfoMetodosPorProjetos(
+			getProjetosParaAnalise(), false);
+		
+		FiltrarMetodosLongos filtrarML = new FiltrarMetodosLongos();
+		ArrayList<DadosMetodoLongo> metodosLongos = filtrarML.filtrarPorValorLimiar(
+			classes, valorLimiar);
+		
+		exibeMetodosLongos(metodosLongos, "Filtrar por valor limiar");
+		
 		assertTrue(metodosLongos.size() > 0);
 	}
 
 	@Test
 	public void testFiltrarPorProjetoExemploGeral() {
-		fail("Not yet implemented");
+		AnalisadorProjeto analisadorProjeto = new AnalisadorProjeto();
+		ArrayList<DadosClasse> classes = analisadorProjeto.getInfoMetodosPorProjetos(
+			getProjetosParaAnalise(), false);
+		
+		GerenciadorProjetoExemplo gpe = new GerenciadorProjetoExemplo();
+		int valorLimiarGlobal = gpe.obterValorLimiarGlobal(projetoExemplo, porcentagem);
+		int medianaGlobal = gpe.obterMedianaGlobal(projetoExemplo, porcentagem);
+		
+		FiltrarMetodosLongos filtrarML = new FiltrarMetodosLongos();
+		ArrayList<DadosMetodoLongo> metodosLongos = filtrarML.filtrarPorProjetoExemploGeral(
+				classes, valorLimiarGlobal, medianaGlobal);
+		
+		exibeMetodosLongos(metodosLongos, "Filtrar Por Projeto Exemplo Geral");
+
+		assertTrue(metodosLongos.size() > 0);
 	}
 
 	@Test
 	public void testFiltrarPorProjetoExemploPreocupacaoArquitetural() {
-		fail("Not yet implemented");
+		AnalisadorProjeto analisadorProjeto = new AnalisadorProjeto();
+		ArrayList<DadosClasse> classes = analisadorProjeto.getInfoMetodosPorProjetos(
+			getProjetosParaAnalise(), true);
+		
+		GerenciadorProjetoExemplo gpe = new GerenciadorProjetoExemplo();
+		LinkedList<DadosComponentesArquiteturais> dadosCA = gpe.
+			criarTabelaCompArquiteturais(projetoExemplo, porcentagem);
+		
+		FiltrarMetodosLongos filtrarML = new FiltrarMetodosLongos();
+		ArrayList<DadosMetodoLongo> metodosLongos = filtrarML.
+				filtrarPorProjetoExemploPreocupacaoArquitetural(classes, dadosCA);
+		
+		exibeMetodosLongos(metodosLongos, "Filtrar por projeto exemplo preocupação arquitetural");
+		
+		assertTrue(metodosLongos.size() > 0);
 	}
 
 }
